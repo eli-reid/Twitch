ffd0242c8646b42f443d1ebcd1a9852dbc7dfef3
Eli Reid <elir@elireid.com>
2023-04-30 18:37:27 -0400

    refactored and did a manual test on Apiconnector

--- Twitch/API/_ApiRequest.py
+++ Twitch/API/_ApiRequest.py
@@ -1,98 +1,27 @@
-from typing import Optional,Callable
+from http import HTTPMethod
+from typing import Coroutine, Optional,Callable
 import asyncio
 import aiohttp
 import json
 from aiohttp import ClientResponse, web
-
 from Twitch.CallbackServer import CallbackServer
 
 class APIRequest():
-    def __init__(self, apiURL:str, returnURL:Optional[str]=None, returnPort: Optional[int]=None) -> None:
-        self._httpClientSession = aiohttp.ClientSession()
+    def __init__(self, apiURL:str) -> None:
+        self._seesion = aiohttp.ClientSession() 
         self._apiURL = apiURL
-        self._returnURL = returnURL if returnURL else "http://localhost" 
-        self._returnPort = returnPort if returnPort else 8880
+        self._RequestFunc: dict[HTTPMethod, Callable] = {
+            HTTPMethod.DELETE : self._seesion.delete,
+            HTTPMethod.GET : self._seesion.get,
+            HTTPMethod.PATCH : self._seesion.patch,
+            HTTPMethod.POST : self._seesion.post,
+            HTTPMethod.PUT : self._seesion.put,
+        }
 
-    async def getRequest(self, endPoint:str, headers:Optional[dict[str,str]]=None, callbacks:dict[str,Callable]=None, **kwargs) -> ClientResponse:
-        """
-        getRequest
-            * endPoint -> str: location  of api call 
-            * headers -> dict[str, str]: for any haeaders reqiured by api 
-            * callbacks -> dict[Method, function] 
-                * method: str  "GET", "POST", ...
-                * funcion: async callback(request) - request accepts a aiohttp.web.Request
-        """
-        if callbacks is not None:
-            callbackServer = CallbackServer("/callback", callbacks, self._returnURL, self._returnPort)
-            await callbackServer.start()
-        response = await self._httpClientSession.get(f"{self._apiURL + endPoint}",headers=headers)
-        await self._httpClientSession.close()
-        return response
-
-    async def postRequest(self, endPoint:str, headers:Optional[dict]=None, callbacks:dict[str,Callable]=None, **kwargs) -> ClientResponse:
-        """
-        postRequest
-            * endPoint -> str: location  of api call 
-            * headers -> dict[str, str]: for any haeaders reqiured by api 
-            * callbacks -> dict[Method, function] 
-                * method: str  "GET", "POST", ...
-                * funcion: async callback(request) - request accepts a aiohttp.web.Request
-        """
-        if len(callbacks.keys()):
-            callbackServer = CallbackServer("/callback", callbacks, self._returnURL, self._returnPort)
-            await callbackServer.start()
-        response = await self._httpClientSession.post(url=self._apiURL + endPoint, headers=headers, kwargs=kwargs)
-        await self._httpClientSession.close()
-        return response
-
-    async def patchRequest(self, endPoint:str, headers:Optional[dict[str,str]]=None, callbacks:dict[str,Callable]=None, **kwargs) -> ClientResponse:
-        """
-        getRequest
-            * endPoint -> str: location  of api call 
-            * headers -> dict[str, str]: for any haeaders reqiured by api 
-            * callbacks -> dict[Method, function] 
-                * method: str  "GET", "POST", ...
-                * funcion: async callback(request) - request accepts a aiohttp.web.Request
-        """
-        if len(callbacks.keys()):
-            callbackServer = CallbackServer("/callback", callbacks, self._returnURL, self._returnPort)
-            await callbackServer.start()
-        response = await self._httpClientSession.patch(url=self._apiURL + endPoint, headers=headers, kwargs=kwargs)
-        await self._httpClientSession.close()
-        return response 
-
-    async def deleteRequest(self, endPoint:str, headers:Optional[dict[str,str]]=None, callbacks:dict[str,Callable]=None, **kwargs) -> ClientResponse:
-        """
-        getRequest
-            * endPoint -> str: location  of api call 
-            * headers -> dict[str, str]: for any haeaders reqiured by api 
-            * callbacks -> dict[Method, function] 
-                * method: str  "GET", "POST", ...
-                * funcion: async callback(request) - request accepts a aiohttp.web.Request
-        """
-        if len(callbacks.keys()):
-            callbackServer = CallbackServer("/callback", callbacks, self._returnURL, self._returnPort)
-            await callbackServer.start()
-        response = await self._httpClientSession.delete(url=self._apiURL + endPoint, headers=headers, kwargs=kwargs)
-        await self._httpClientSession.close()
-        return response 
-
-    async def putRequest(self, endPoint:str, headers:Optional[dict[str,str]]=None, callbacks:dict[str,Callable]=None, **kwargs) -> ClientResponse:
-        """
-        getRequest
-            * endPoint -> str: location  of api call 
-            * headers -> dict[str, str]: for any haeaders reqiured by api 
-            * callbacks -> dict[Method, function] 
-                * method: str  "GET", "POST", ...
-                * funcion: async callback(request) - request accepts a aiohttp.web.Request
-        """
-        if len(callbacks.keys()):
-            callbackServer = CallbackServer("/callback", callbacks, self._returnURL, self._returnPort)
-            await callbackServer.start()
-        response = await self._httpClientSession.put(url=self._apiURL + endPoint, headers=headers, kwargs=kwargs)
-        
-        self._httpClientSession.close()
-        return response       
-
+    async def request(self, endPoint:str, method:HTTPMethod, headers:Optional[dict[str,str]]=None, params=None)-> ClientResponse:
+                response = await self._RequestFunc[method](f"{self._apiURL + endPoint}", headers=headers, params=params)
+                await asyncio.sleep(0)
+                return response
+    
     async def close(self):
-        await self._httpClientSession.close();
+           await self._seesion.close()
